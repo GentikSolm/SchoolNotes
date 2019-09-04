263ed4bf7662749c4d1e105a63dd76b7089fbfa2
Joshua Brown <josh@mjbrowns.com>
2019-09-03 20:20:04 -0400

    Finished cs1 lab and saved other changes from the day

---
+++ Computer Science I/main.cpp
@@ -0,0 +1,15 @@
+/* Lab 1 : main.cpp
+Author: Joshua Brown
+jgb38@uakron.edu
+Purpose: Demonstration of a simple program */
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Calculated total pay and output"
+  // variables for payrate and hours
+  int payrate = 7;
+  int hours = 40;
+  int totalPay = payrate * hours;
+  cout << "Total pay: $" << totalPay << ".00" << endl;
+}
