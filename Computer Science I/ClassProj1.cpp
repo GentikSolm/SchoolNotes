4c6180b3f7223e2cc09ae348ee142d5cc34ccf99
Joshua Brown <josh@mjbrowns.com>
2019-09-05 18:56:19 -0400

    More CS class stuff

---
+++ Computer Science I/ClassProj1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  cout << "Mike asked: Bob, \"where is the file spChar.cpp\"?\n";
+  cout << R"(Bob answered: it is in a network drive under the folder "S:\\comp\netwk\tiger")";
+  return 0;
+}
