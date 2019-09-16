4543f598892694e7d2c32284edcfa617b4aef20c
Joshua Brown <josh@mjbrowns.com>
2019-09-16 13:35:55 -0400

    Finished ASSGN2-B

--- Computer Science I/CurrentProj/ASSGN2-B.cpp
+++ Computer Science I/CurrentProj/ASSGN2-B.cpp
@@ -9,6 +9,25 @@
 #include <iostream>
 using namespace std;
 int main(){
+	cout << "Please enter 10 numbers!\n";
 
-  
+	//Declaring needed variables
+	int i = 0;
+	int pos = 0;
+	int neg = 0;
+	int temp = 0;
+
+	//WHile loop that takes in 10 numbers
+	while(i < 10){
+		cout << "\nNumber " << i + 1 << ": ";
+		cin >> temp;
+		if(temp > 0) pos += temp;
+		else neg += temp;
+		i++;
+	};
+
+	//Returns the final values of positive and negative
+  cout << "Total Negative sum: " << neg << ".\n";
+	cout << "Total Positive sum: " << pos << ".\n";
+	cout << "Total sum of Positive and Negative: " << pos + neg << '.';
 }
