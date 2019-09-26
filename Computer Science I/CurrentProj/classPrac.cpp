0e3a83d9c70f4477bacef062db1543f7d7c960e2
Joshua Brown <josh@mjbrowns.com>
2019-09-26 18:54:15 -0400

    learned time

--- Computer Science I/CurrentProj/classPrac.cpp
+++ Computer Science I/CurrentProj/classPrac.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <ctime>
 using namespace std;
 int main(){
 
 	//Seed changes random output!
-	srand(293);
+	srand(time(0));
 	const int myCounter = 127;
 	int varCtr;
 	for(int varCtr = 0; varCtr <= myCounter; varCtr++){
