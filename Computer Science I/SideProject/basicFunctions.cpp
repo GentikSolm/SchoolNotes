a8844de76e1a0a4c1b90b02796fb44a07b98de63
Joshua Brown <josh@mjbrowns.com>
2019-10-02 12:19:49 -0400

    created header fule

--- Computer Science I/SideProject/basicFunctions.cpp
+++ Computer Science I/SideProject/basicFunctions.cpp
@@ -6,38 +6,13 @@
   Version 1.01 9.29.2019
   Purpose: Some simple functions that are used in general programs
 */
-#include <iostream>
-#include <limits>
-#include <iomanip>
-#include <string>
-#include <ctime>
-using std::cin;
-using std::cout;
-using std::string;
 
-// The following functions are for input verification.
-int userInInt(string termin);
+int userInInt(string termin = "\n-> "){
 	// Requests and int from the user and then verifies it is an int.
 	// The validated user input is then returned
 	// Non-requred string termin, is what is displayed when asking for the variable.
 	// termin is ONLY used if input is invalid
 	// NOTE: NO OUTPUT TO USER UNLESS INVALID INPUT
-
-double userInDouble(string termin);
-	// Requests Double from the user and then verfies it is a double.
-	// The validated user input is then returned
-	// Non-requred string termin, is what is displayed when asking for the variable.
-	// termin is ONLY used if input is invalid
-	// NOTE: NO OUTPUT TO USER UNLESS INVALID INPUT
-
-// Returns a random number between the given inputs, max and min
-int randBetween(int max, bool noError);
-	// Returns a number that is between the Max and 1. If Max <= 1,
-		// this will cout error and return -1.
-	// If the function should be used in a case where it is okay for Max <= 1, pass in noError = true
-		// this will cause the program to not error and return a value that is in absoulute value of the range.
-
-int userInInt(string termin = "\n-> "){
 	int temp;
 	cin >> temp;
 	while(cin.fail()){
@@ -50,6 +25,11 @@ int userInInt(string termin = "\n-> "){
 }
 
 double userInDouble(string termin = "\n-> "){
+	// Requests Double from the user and then verfies it is a double.
+	// The validated user input is then returned
+	// Non-requred string termin, is what is displayed when asking for the variable.
+	// termin is ONLY used if input is invalid
+	// NOTE: NO OUTPUT TO USER UNLESS INVALID INPUT
 	double temp;
 	cin >> temp;
 	while(cin.fail()){
@@ -62,6 +42,11 @@ double userInDouble(string termin = "\n-> "){
 }
 
 int randBetween(int max = 10, bool noError = false){
+	// Returns a number that is between the Max and 1. If Max <= 1,
+		// this will cout error and return -1.
+	// If the function should be used in a case where it is okay for Max <= 1, pass in noError = true
+		// this will cause the program to not error and return a value that is in absoulute value of the range.
+
 	srand(time(0));
 	int num = 0, min = 1;
 	int randRange = max - min;
--- Computer Science I/SideProject/testProg.cpp
+++ Computer Science I/SideProject/testProg.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "basicFuncHeader.h"
 #include "basicFunctions.cpp"
 using namespace std;
 
