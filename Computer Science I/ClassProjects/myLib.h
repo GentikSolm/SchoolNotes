0ab88d071d82a4478658cf4c81e5d7457d927de1
Josh <josh@mjbrowns.com>
2019-10-28 23:39:26 -0400

    prep for assgn-6

--- Computer Science I/ClassProjects/myLib.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYLIB_H
-#define MYLIB_H
-#include <string>
-namespace cs1{
-	void fileCopy(std::string sourceFile, std::string targetFile);
-	// does file copy regardless of file type
-}
-
-#endif
