07cbb37c716f93159f0d563ac0ec5b47fab404f0
Joshua Brown <josh@mjbrowns.com>
2019-11-07 10:46:59 -0500

    Made qui in latin and also figued out how to do progress bars

--- Computer Science I/CurrentProj/test.cpp
+++ Computer Science I/CurrentProj/test.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <ctime>
+#include <thread>
+#include <chrono>
 using namespace std;
 int main(){
-	int first = 5, last = 15;
-	int *squares = new int[last-first+1];
-	for(int i = 0; i <= last -first; i++){
-		squares[i] = (first + i) * (first + i);
-	}
-	for(int i = 0; i <= 10; i++){
-		cout << squares[i] << "\n";
-	}
+	float progress = 0.0;
+	while (progress <= 1.0) {
+	    int barWidth = 70;
 
+	    std::cout << "[";
+	    int pos = barWidth * progress;
+	    for (int i = 0; i < barWidth; ++i) {
+	        if (i < pos) std::cout << "=";
+	        else if (i == pos) std::cout << ">";
+	        else std::cout << " ";
+	    }
+	    std::cout << "] " << int(progress * 100.0) << " %\r";
+	    std::cout.flush();
+
+		this_thread::sleep_for(chrono::seconds(1));
+	    progress += 0.16; // for demonstration only
+		if(progress >= 1.0){
+			cout << "[";
+			for(int i = 0; i < 70; i++){
+				cout << "=";
+			}
+			cout << "] 100 %";
+			break;
+		}
+	}
+	std::cout << std::endl;
 }
