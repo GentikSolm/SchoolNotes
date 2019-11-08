9d3c7dc2b684c95346e22ada8210123b47ef17e7
Joshua Brown <josh@mjbrowns.com>
2019-11-07 19:19:34 -0500

    Reverted progbar

--- Computer Science I/UsefulLibs/test.cpp
+++ Computer Science I/UsefulLibs/test.cpp
@@ -7,14 +7,8 @@
 #include <thread>
 using namespace std;
 int main(){
-	progBar(19);
-	for(int i = 0; i < 19; i++){
-		progBar();
-		std::this_thread::sleep_for(chrono::milliseconds(70));
-	}
-	cout << endl;
-	for(int i = 0; i < 19; i++){
-		progIcon(19);
+	for(int i = 0; i < 200; i++){
+		progBar(19);
 		std::this_thread::sleep_for(chrono::milliseconds(70));
 	}
 }
