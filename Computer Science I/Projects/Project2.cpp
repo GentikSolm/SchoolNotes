a5837a7c97ef7f6e0ddea4d87832735fb1626d5c
Joshua Brown <josh@mjbrowns.com>
2019-11-14 17:51:53 -0500

    Finally done with project!

--- Computer Science I/Projects/Project2.cpp
+++ Computer Science I/Projects/Project2.cpp
@@ -95,25 +95,31 @@ int newPatientInput(int);
 
 
 int main(){
-	char ** vacArr;
-	char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE];
-	int totalVacant = 0, userInput;
-	readGrid(hospitalFloors);
-	checkOuts(hospitalFloors);
+	char ** vacArr; // dynamic array of pointers to char
+	char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE]; // grid for hospital Floors and rooms
+	int totalVacant = 0, userInput; // Total vacant rooms and storage for user input
+
+	readGrid(hospitalFloors); //Read file
+	checkOuts(hospitalFloors); // checkout all checkouts
 	vacArr = tranfers(hospitalFloors, totalVacant);
 	cout << " Checkouts and transfers completed\n";
+	cout << "Please enter -1 to quit early.\n";
+	//While there is still room for more patients
 	while(totalVacant != 0){
 		printGrid(hospitalFloors);
 		userInput = newPatientInput(totalVacant);
+		//Case for if the user wants to quit early
 		if(userInput == -1) break;
 		newPatient(userInput, totalVacant, vacArr);
 	}
 	printGrid(hospitalFloors);
-	delete vacArr;
+	delete vacArr; //Delete dynamic array
+	//If program ended properly
 	if(totalVacant == 0){
 		cout << "*** No more rooms are available today ***";
 		return 4;
 	}
+	//If early quit
 	else{
 		cout << "*** More rooms are available today ***";
 		return 0;
@@ -130,6 +136,7 @@ Prints the grid to the screen
 */
 void printGrid(const char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE]){
 	cout << ' ' << std::setfill('-') << std::setw(22) << '-' << endl;
+	//Loop every floor and room
 	for(int i = 0; i < FLOOR_ARRAY_SIZE; i++){
 		for(int j = 0; j < ROOM_ARRAY_SIZE; j++){
 			cout << ' ' << hospitalFloors[i][j] << ' ';
@@ -150,15 +157,19 @@ Reads in the initial grid from Beds.txt file
 void readGrid(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE]){
 	ifstream bedFile;
 	int inputNum;
+	//Array of the different constant characters for easy access
 	char whatIcon[7] = {VACANT, CHECKOUT, OCCUPIED, NURSE_STATION, TRANSFER, UTILITY, INVALID};
 	bedFile.open("Beds.txt");
+	//File verification
 	if(bedFile.fail()){
 		cout << "File failed!";
 		exit(EXIT_FAILURE);
 	}
+	//Loop every floor and print it to console
 	for(int i = 0; i < FLOOR_ARRAY_SIZE; i++){
 		for(int j = 0; j < ROOM_ARRAY_SIZE; j++){
 			bedFile >> inputNum;
+			//Takes care of invalid input
 			if(inputNum >= 7 || inputNum < 0) hospitalFloors[i][j] = whatIcon[7];
 			else{
 				hospitalFloors[i][j] = whatIcon[inputNum];
@@ -166,6 +177,7 @@ void readGrid(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE]){
 		}
 	}
 	bedFile.close();
+	//Prints grid again
 	printGrid(hospitalFloors);
 	cout << " Grid Loaded\n\n";
 }
@@ -176,6 +188,7 @@ Post:	Sets all checkout rooms to vacant
 Perform the checkouts
 */
 void checkOuts(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE]){
+	//Loop through grid and take out checkots
 	for(int i = 0; i < FLOOR_ARRAY_SIZE; i++){
 		for(int j = 0; j < ROOM_ARRAY_SIZE; j++){
 			if(hospitalFloors[i][j] == CHECKOUT){
@@ -194,6 +207,7 @@ Perform the transfers and build the dynamic array of pointers.
 this function also will compute the available number of rooms
 */
 char** tranfers(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE], int &vacantCount){
+	//Loop and find all vacant
 	int vacArrCounter = 0;
 	for(int i = 0; i < FLOOR_ARRAY_SIZE; i++){
 		for(int j = 0; j < ROOM_ARRAY_SIZE; j++){
@@ -202,16 +216,22 @@ char** tranfers(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE], int &vac
 			}
 		}
 	}
+	//Create the dynamic array
 	char** vacArr = new char*[vacantCount];
+	//This and previous loop cannot be combined, because dynamic array must be initialized
+	//With the total ammount of vacant rooms
 	for(int i = 0; i < FLOOR_ARRAY_SIZE; i++){
 		for(int j = 0; j < ROOM_ARRAY_SIZE; j++){
 			if(hospitalFloors[i][j] == VACANT){
+				//Builds array of pointers
 				vacArr[vacArrCounter] = &hospitalFloors[i][j];
 				vacArrCounter++;
 			}
 		}
 	}
-	char* temp;
+	//Loops and swaps each transfer with a vacant starting from the back of the
+	// vacant list
+	//NOTE: when swaped, transfer changes to OCCUPIED, and the pointer is still vacant.
 	for(int i = FLOOR_ARRAY_SIZE-1; i >= 0; i--){
 		for(int j = ROOM_ARRAY_SIZE-1; j >= 0 ; j--){
 			if(hospitalFloors[i][j] == TRANSFER){
@@ -219,7 +239,6 @@ char** tranfers(char hospitalFloors[FLOOR_ARRAY_SIZE][ROOM_ARRAY_SIZE], int &vac
 				vacArrCounter--;
 				std::swap(*vacArr[vacArrCounter], hospitalFloors[i][j]);
 				vacArr[vacArrCounter] = &hospitalFloors[i][j];
-				//hospitalFloors[i][j] = OCCUPIED;
 			}
 		}
 	}
@@ -238,8 +257,12 @@ the nurse station and passing the dynamic array of vacancies count for updating
 */
 void newPatient(int newPatients, int &roomsLeft, char **arrOfRoomsLeft){
 	for(int i = 0; i < newPatients; i++){
+		//take one from total vacant rooms
+		//Must be done before so that no out of index errors.
 		--roomsLeft;
+		//Sets value to OCCUPIED
 		*arrOfRoomsLeft[roomsLeft] = OCCUPIED;
+		//sets pointer to nullptr
 		arrOfRoomsLeft[roomsLeft] = nullptr;
 	}
 }
@@ -254,6 +277,7 @@ int newPatientInput(int minimum){
 	cout << "Please enter the number of new patients, from one up to " << minimum << ": ";
 	int inputNum;
 	cin >> inputNum;
+	//while not valid and not in range
 	while(cin.fail() || inputNum > minimum || inputNum <= 0){
 		cin.clear();
 		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
