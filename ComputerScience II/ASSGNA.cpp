720eca8d43784957e630884358f0d0becad309a6
Joshua Brown <josh@mjbrowns.com>
2020-01-16 13:53:04 -0500

    I HATE ASSGNA

--- ComputerScience II/ASSGNA.cpp
+++ ComputerScience II/ASSGNA.cpp
@@ -1,9 +1,22 @@
+/*
+  COPYRIGHT (C) 2019 Joshua Brown (jgb38) All rights reserved.
+  ASSGNA
+  Auther. Josh Brown
+		  jgb38@zips.uakron.edu
+  Version 1.01 1.16.2020
+  Purpose: Delete repeats in an array
+*/
+
+
 #include <iostream>
 using std::unique_ptr;
 using std::cout;
-unique_ptr<char[]> deleteRepeats(char*);
+const int SIZE = 10;
+unique_ptr<char[]> deleteRepeats(const char (&ogArray)[SIZE]);
+	//Takes in a character array and returns a unique pointer to
+	// an array containing only the unique values
 
-int const SIZE = 10;
+
 int main(){
 	char origionalArray[SIZE];
 	origionalArray[0] = 'a';
@@ -19,23 +32,30 @@ int main(){
 	unique_ptr<char[]>noRepeats;
 	noRepeats = deleteRepeats(origionalArray);
 
-	for(char& letter : noRepeats){
-		cout << letter << std::endl;
-	}
+	// for(int i = 0; i < SIZE; i++){
+	//
+	// }
 }
 
-unique_ptr<char[]> deleteRepeats(char* ogArray){
+unique_ptr<char[]> deleteRepeats(const char (&ogArray)[SIZE]){
+	// Arrays decay when they are passed into functions
+	// Therefore, it is not possible to use a range based loop on them
+	// when passed. Because of the decay they lose the
+	// size data origionally attached to them.
 	char tempArray[SIZE] = {'\0'};
-	bool flag;
+	// bool flag;
 	int counter = 0;
-	for(int i = 0; i < SIZE; i++){
-		flag = false;
-		for(int j = 0; j < SIZE; j++){
-			if(ogArray[i] == tempArray[j])flag = true;
-		}
-		if(!flag){
-			tempArray[counter++] = ogArray[i];
-		}
+	// for(int i = 0; i < SIZE; i++){
+	// 	flag = false;
+	// 	for(int j = 0; j < SIZE; j++){
+	// 		if(ogArray[i] == tempArray[j])flag = true;
+	// 	}
+	// 	if(!flag){
+	// 		tempArray[counter++] = ogArray[i];
+	// 	}
+	// }
+	for(char item : ogArray){
+		cout << counter++ << std::endl;
 	}
 
 	unique_ptr<char[]>newArray(new char[counter]);
