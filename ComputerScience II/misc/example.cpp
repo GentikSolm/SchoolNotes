8b17b2122f1265cee8090666cd39a3496317927e
Josh <josh@mjbrowns.com>
2020-03-04 13:12:47 -0500

    Lots of website work

--- ComputerScience II/misc/example.cpp
+++ ComputerScience II/misc/example.cpp
@@ -5,6 +5,5 @@ using std::endl;
 using std::setw;
 const int SIZE = 40;
 int main(){
-    cout << setw(SIZE) << std::setfill('|') << std::right << "Hello world!\n";
-    cout << setw(SIZE) << std::setfill('-') << std::left << "Goodbye world!";
+    printf("Hello world!", '+');
 }
--- Web/public/header.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="localhost:8080/style.css" type="text/css"/>
-<link rel="icon" href="/favicon.ico" type="image/x-icon" />
