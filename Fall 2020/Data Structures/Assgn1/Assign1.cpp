4c7a13880c56e0feaf53426a6263d3d7ac131f37
josh <josh@mjbrowns.com>
2020-09-12 02:21:09 -0400

    Done, just need to do times and write that paper and use rest of time for comments

--- Fall 2020/Data Structures/Assgn1/Assign1.cpp
+++ Fall 2020/Data Structures/Assgn1/Assign1.cpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <cmath>
 #include <fstream>
+#include <iomanip>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,7 +13,6 @@ using std::string;
 
 //TODO:
 /*
-    Linked list implementation
     Times
     add quick notes
 */
@@ -47,13 +47,14 @@ public:
         //Delete city from db by name
     void printNearby(const string cName, const float distance);
         //print nearby locations
-    void printDb(const const cityLinked* location);
+    void printDb(const cityLinked* location);
     void printDbFull();
 
     cityLinked* searchDbName(const string cName);
         //search db by name
     cityLinked* searchDbCoord(const float xcord, float ycord);
         //search db by location
+    ~CityLinkedList();
 };
 class CityArray{
 private:
@@ -91,32 +92,36 @@ float userInfloat();
 
 int main()
 {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(7);
     int choice;
     char letter;
     CityArray ArrayDb;
+    CityLinkedList LinkedDb;
     float x,y;
     int location;
+    cityLinked* linkedLocation;
     string name;
     std::ifstream database;
     database.open("Database.txt");
     while(!database.eof()){
         database >> name >> x >> y;
         ArrayDb.addCity(x, y, name,1);
+        LinkedDb.addCity(x, y, name,1);
     }
     database.close();
     do{
         printMenu();
         cout << "Enter your implementation option: ";
         cin >> letter;
-        while(letter != 'A' && letter != 'b'){
+        while(letter != 'A' && letter != 'B' && letter != 'a' && letter != 'b'){
             cout << "That was not a valid letter! Please enter another letter: ";
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             cin >> letter;
         }
         cout << "Enter your operation option: ";
         choice = userInInt();
-        if(letter == 'A'){
-
+        if(letter == 'A' || letter == 'a'){
             switch (choice) {
                 case 1://Insert
                     name = getName();
@@ -156,21 +161,43 @@ int main()
                 break;
             }
         }
-        else if(letter == 'B'){
+        else if(letter == 'B' || letter == 'b'){
             switch (choice) {
-                case 1:
+                case 1://Insert
+                    name = getName();
+                    x = getx();
+                    y = gety();
+                    LinkedDb.addCity(x, y, name,0);
+
                 break;
-                case 2:
+                case 2://search name
+                    name = getName();
+                    linkedLocation = LinkedDb.searchDbName(name);
+                    LinkedDb.printDb(linkedLocation);
                 break;
-                case 3:
+                case 3://search coord
+                    x = getx();
+                    y = gety();
+                    linkedLocation = LinkedDb.searchDbCoord(x,y);
+                    LinkedDb.printDb(linkedLocation);
                 break;
-                case 4:
+                case 4://delete name
+                    name = getName();
+                    LinkedDb.deleteCityName(name);
                 break;
-                case 5:
+                case 5://delete coord
+                    x = getx();
+                    y = gety();
+                    LinkedDb.deleteCityCoord(x,y);
                 break;
-                case 6:
+                case 6://list all in area
+                    name = getName();
+                    cout << "Enter Distance: ";
+                    y = userInfloat();
+                    LinkedDb.printNearby(name, y);
                 break;
-                case 7:
+                case 7://print entire db
+                    LinkedDb.printDbFull();
                 break;
             }
         }
@@ -191,7 +218,7 @@ int main()
 //Array functions
 void CityArray::addCity(const float  xcord, const float ycord, const string cName, const bool init){
     // Add city into db, true for success and false for city already present
-    if(CityArray::searchDbName(cName) != nullptr && !init){
+    if(CityArray::searchDbName(cName) != -1 && !init){
         cout << "No need to insert again, as this record exists in the existing data set.\n\n";
         return;
     }
@@ -246,6 +273,7 @@ void CityArray::printNearby(const string cName, const float distance){
     y = dec_to_rad(cityDb[arrLoc].ycord);
     cout << "Output:\n";
     for(int i = 0; i < size; i++){
+        if(cityDb[i].city == cName) continue; //skips self
         x1 = dec_to_rad(cityDb[i].xcord);
         y1 = dec_to_rad(cityDb[i].ycord);
         distanceBetween = 3963 * acos(sin(x)*sin(x1)+cos(x)*cos(x1)*cos(y1-y));
@@ -281,8 +309,8 @@ int CityArray::searchDbCoord(const float xcord, float ycord){
 }
 
 //Linked list functions
-void addCity(const float  xcord, const float ycord, const string cName, const bool init){
-    if(CityLinkedList::searchDbName(cName) == -1){
+void CityLinkedList::addCity(const float  xcord, const float ycord, const string cName, const bool init){
+    if(CityLinkedList::searchDbName(cName) == nullptr){
         cityLinked* temp;
         temp = head;
         head = new cityLinked;
@@ -297,16 +325,75 @@ void addCity(const float  xcord, const float ycord, const string cName, const bo
         cout << "No need to insert again, as this record exists in the existing data set.\n\n";
     }
 }
-void deleteCityName(const string cName){
+void CityLinkedList::deleteCityName(const string cName){
+    cityLinked* temp = head;
+    cityLinked* temp1;
+    if(temp == nullptr){
+        cout << "No such record exists in the existing data set.\n\n";
+    } else if(temp->city == cName) {
+        delete temp;
+        temp = nullptr;
+    }
+    while(temp->next!=nullptr){
+        if(temp->next->city == cName){
+            temp1 = temp->next;
+            temp->next = temp->next->next;
+            delete temp1;
+            cout << "Deleted successfully" << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "No such record exists in the existing data set.\n\n";
+}
+void CityLinkedList::deleteCityCoord(const float xcord, const float ycord){
+    cityLinked* temp = head;
+    cityLinked* temp1;
+    if(temp == nullptr){
+        cout << "No such record exists in the existing data set.\n\n";
+    } else if(temp->xcord == xcord && temp->ycord == ycord) {
+        delete temp;
+        temp = nullptr;
+    }
+    while(temp->next!=nullptr){
+        if(temp->next->xcord == xcord && temp->next->ycord == ycord){
+            temp1 = temp->next;
+            temp->next = temp->next->next;
+            delete temp1;
+            cout << "Deleted successfully" << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "No such record exists in the existing data set.\n\n";
+}
+void CityLinkedList::printNearby(const string cName, const float distance){
+    cityLinked* linkedLoc = CityLinkedList::searchDbName(cName);
+    cityLinked* temp = head;
+    if(linkedLoc == nullptr){
+        cout << "No such record exists in the existing data set.\n";
+        return;
+    }
+    float x,y,x1,y1, distanceBetween;
+    x = dec_to_rad(linkedLoc->xcord);
+    y = dec_to_rad(linkedLoc->ycord);
+    cout << "Output:\n";
+    while(temp!=nullptr ){
+        if(temp->city == cName){
+            temp = temp->next;
+            continue;
+        } //skips self
+        x1 = dec_to_rad(temp->xcord);
+        y1 = dec_to_rad(temp->ycord);
+        distanceBetween = 3963 * acos(sin(x)*sin(x1)+cos(x)*cos(x1)*cos(y1-y));
+        if(distanceBetween <= distance){
+            cout << temp->city << ", (" << temp->xcord << ", " << temp->ycord << ')' << endl;
+        }
 
+        temp = temp->next;
+    }
 }
-void deleteCityCoord(const float xcord, const float ycord){
-
-}
-void printNearby(const string cName, const float distance){
-    
-}
-cityLinked* searchDbName(const string cName){
+cityLinked* CityLinkedList::searchDbName(const string cName){
     cityLinked* temp = head;
     while(temp!=nullptr){
         if(temp->city == cName){
@@ -316,7 +403,7 @@ cityLinked* searchDbName(const string cName){
     }
     return nullptr;
 }
-cityLinked* searchDbCoord(const float xcord, float ycord){
+cityLinked* CityLinkedList::searchDbCoord(const float xcord, float ycord){
     cityLinked* temp = head;
     while(temp!=nullptr){
         if(temp->xcord == xcord && temp->ycord == ycord){
@@ -326,19 +413,31 @@ cityLinked* searchDbCoord(const float xcord, float ycord){
     }
     return nullptr;
 }
-void printDb(const cityLinked* location){
+void CityLinkedList::printDb(const cityLinked* location){
     cout << "Output:\n";
-    cout << location->name << ", (" << location->xcord << ", " << location->ycord << ')' << endl;
+    cout << location->city << ", (" << location->xcord << ", " << location->ycord << ')' << endl;
 }
-void printDbFull(){
+void CityLinkedList::printDbFull(){
     cityLinked* temp = head;
     cout << "Output:\n";
     while(temp!=nullptr){
-        cout << temp->name << ", (" << temp->xcord << ", " << temp->ycord << ')' << endl;
+        cout << temp->city << ", (" << temp->xcord << ", " << temp->ycord << ')' << endl;
         temp = temp->next;
     }
 
 }
+CityLinkedList::~CityLinkedList(){
+    cityLinked* temp = head;
+    while(temp->next!=nullptr){
+        head = head->next;
+        delete temp;
+        temp = head;
+    }
+    delete temp;
+}
+
+
+
 //General Functions
 float dec_to_rad(float degree){
     float M_PI = 3.1415926535;
@@ -359,7 +458,7 @@ int userInInt(){
 	int temp;
 	cin >> temp;
 	while(cin.fail() || temp < 1 || temp > 8){
-		cout << "That was not a valid option! Please enter a number\n::";
+		cout << "That was not a valid option! Please enter a number: ";
 		cin.clear();
 		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		cin >> temp;
