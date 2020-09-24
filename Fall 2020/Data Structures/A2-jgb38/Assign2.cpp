b04015ef08262cdc7a1f42af11b2b943b919a973
josh <josh@mjbrowns.com>
2020-09-24 02:01:08 -0400

    DONE WITH ASSGN2

--- Fall 2020/Data Structures/A2-jgb38/Assign2.cpp
+++ Fall 2020/Data Structures/A2-jgb38/Assign2.cpp
@@ -6,9 +6,7 @@
 #include <fstream>
 #include <set>
 #include <vector>
-#include <map>
 using std::set;
-using std::map;
 using std::vector;
 using std::ifstream;
 using std::string;
@@ -18,36 +16,48 @@ using std::cout;
 
 string FILENAME = "Dictionary.txt";
 void getWord(string &);
-bool wordLadder(string, string, set<string> &, map<string,vector<string>> &);
-bool output(string&, string, vector<string>&, map<string,vector<string>>&);
+    //Gets word from user, string var must be referenceable.
+    //makes sure there is no non letters in input.
 
+bool wordLadder(string start, string last, set<string> dict);
+    //start and last must be same length.
+    //Dict does not even requre words, will just return false if no linking words are found
+    //returns true if a ladder is found.
+    //In case of ladder, will print out the ladder.
+
+struct scoredWords{
+    int score = 0;
+    string word;
+};
+//Holds a word as well as the words score in relativity to the end target
 int main(){
-    map<string,vector<string> > map;
     string start, end;
-    bool flag;
     cout << "Please enter the starting word: ";
     getWord(start);
     cout << "Please enter the end word: ";
     getWord(end);
+
     ifstream dictionary;
     set<string> dict;
     string temp;
+    int startSize = start.size();
+
+    //makes sure words are same length before loading dict.
+    if(start.size()!= end.size()){
+        cout << "Words must match in length!";
+        return 0;
+    }
+
+    //Generates dict with only words of length of start/end
     dictionary.open(FILENAME);
     while(!dictionary.eof()){
         dictionary >> temp;
-        if(temp.size() == start.size()) dict.insert(temp);
+        if(temp.size() == startSize) dict.insert(temp);
     }
-
-    if(dict.find(end) == dict.end()){
-        cout << "Word is not in dictionary!\n";
-    }
-
-    else if(start.size()!= end.size()){
-        cout << "Words must match in length!";
-    }
-
-    flag = wordLadder(start, end, dict, map);
-    if(!flag){
+    //ensures end is in dict, doesnt duplicate because of set properties
+    dict.insert(end);
+    //if wordladder returns true, program exits, else no path was faound.
+    if(!wordLadder(start, end, dict)){
         cout << "No path was found!";
     }
     //Placed ontop of close bracket because do{}while()
@@ -59,73 +69,62 @@ void getWord(string &word){
     while(!flag){
         cin >> word;
         for(char& c : word){
+            //Makes sure string is only letters
             if(!isalpha(c)){
                 cout << "Not a valid word, please remove any non letters!\n";
                 flag = 0;
                 break;
             }
+            //forces all letters to lowercase.
             c = tolower(c);
             flag = 1;
         }
     }
 
 }
+bool wordLadder(string start,string last, set<string> dict){
+    if (last==start){
+        //end case
+        cout << last << endl;
+        return true;
+    }else{
+        string str = last;
+        //we use last instead of start so that the list will print out with the start first.
 
-bool wordLadder(string start, string end, set<string> &dict, map<string,vector<string>> &mp){
-    vector<string> path;
-    set<string> cur_lev;
-    cur_lev.insert(start);
-    set<string> next_lev;
-    path.push_back(end);
+        vector<scoredWords> level;
+        //used to map out current level word possibilites.
+        auto iter = level.begin();
+        for (int i = 0; i < str.size(); i++){
+            str = last;
+            for(char j = 'a'; j<='z'; j++){
+                str[i]=j;
+                if(dict.find(str) != dict.end()){
+                    //This nested loop Generates/finds all words that are 1 letter different
+                    //than 'last'
 
-    while (true){
-        for (auto it = cur_lev.begin();it!=cur_lev.end();it++){
-            dict.erase(*it);
-        } //delete previous level words
-
-        for (auto it = cur_lev.begin();it!=cur_lev.end();it++){  //find current level words
-            //findDict2(*it,dict,next_lev);
-            string str = *it;
-            for (int i = 0; i < str.size(); i++){
-                str = *it;
-                for(char j = 'a'; j<='z'; j++){
-                    str[i]=j;
-                    if(dict.find(str) != dict.end()){
-                        next_lev.insert(str);
-                        mp[str].push_back(*it);
+                    dict.erase(str); //erases word from dict.
+                    iter = level.begin();
+                    scoredWords temp;
+                    temp.word = str;
+                    for(int i = 0; i < start.size(); i++){
+                        if(start[i] == str[i]) temp.score++;
+                        //finds and scores each word to see how far away it is from target
                     }
+                    while(iter != level.end() && iter->score > temp.score) iter++;
+                    level.insert(iter, temp);
+                    //this while loop inserts the scoredWords element in order of rank.
                 }
             }
         }
-        if (next_lev.empty()){
-            return 0;
-        }
-
-        if (next_lev.find(end)!=next_lev.end()){ //if find end string
-            output(start, end, path, mp);
-            return 1;
-        }
-
-        cur_lev.clear();
-        cur_lev = next_lev;
-        next_lev.clear();
-    }
-    return 0;
-}
-
-bool output(string &start,string last, vector<string>& path, map<string, vector<string>>&map){
-    if (last==start){
-        reverse(path.begin(),path.end());
-        for(auto it = path.begin(); it!=path.end(); it++){
-            cout <<*it << endl;
-        }
-        reverse(path.begin(),path.end());
-        return true;
-    }else{
-        for (int i=0;i<map[last].size();i++){
-            path.push_back(map[last][i]);
-            if(output(start,map[last][i], path, map)) return 1;
-            path.pop_back();
+        for(auto iter = level.begin(); iter!= level.end(); iter++){
+            //Transverses the level, if level is empty (no more words or no combinations possible),
+            //then the for loop will instantly terminate and the recursion function will return false,
+            //climbing back up to the previous level, to which it will move to the next item.
+            if(wordLadder(start, iter->word, dict)){
+                //if its true, then a ladder was found and it will print the tree.
+                cout << last << endl;
+                return true;
+            }
         }
     }
     return false;
